96aaf2d1638d07025a1999eb0d7a95a6424a392f
David Canino <canino.david@gmail.com>
2016-11-11 09:44:53 +0100

    added the new 'Example-001'

---
+++ Example-001/src/main.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************************
+ * Examples in the C++ language (the test 'Example-001')
+ *
+ * GitHub repository: http://github.com/davidcanino/C_plus_plus_examples
+ *
+ * Created by David Canino (canino.david@gmail.com), November 2016
+ #
+ # main.cpp - the 'main' file for the test 'Example-001'.
+ **************************************************************************************/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc,char **argv)
+{
+    /* This is the example 'Example-001', near to be the classic 'Hello World!" example! */
+    cout<<endl<<"\tThis is the example 'Example-001' in the C++ language"<<endl<<endl;
+    cout.flush();
+    return EXIT_SUCCESS;
+}

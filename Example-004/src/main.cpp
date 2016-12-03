885186d099ba7dba38d257a4ab09a84c8acd710f
David Canino <canino.david@gmail.com>
2016-12-04 00:11:27 +0100

    modified a little bit the 'Example-004' test

--- Example-004/src/main.cpp
+++ Example-004/src/main.cpp
@@ -3,21 +3,24 @@
  *
  * GitHub repository: http://github.com/davidcanino/C_plus_plus_examples
  *
- * Created by David Canino (canino.david@gmail.com), November 2016
- #
- # main.cpp - the 'main' file for the test 'Example-004'.
+ * Created by David Canino (canino.david@gmail.com), December 2016
+ *
+ * main.cpp - the 'main' file for the test 'Example-004'.
  **************************************************************************************/
 
 #include <iostream>
 #include <cstdlib>
 using namespace std;
 
+/// The main function for the <i>Example-004</i> unit test.
 int main(int argc,char **argv)
 {
     int a[]={-4,5,-44,-5,33,12,55};
     
     /* Here, we consider a very simple C++11 construct! */
-    cout<<endl<<"\tPerformed the automatic C++11 initialization of the array 'a'."<<endl;
+    cout<<endl<<"\tThis is the example 'Example-004' in the C++ language"<<endl<<endl;
+    cout.flush();
+    cout<<"\tPerformed the automatic C++11 initialization of the array 'a'."<<endl;
     cout<<"\tPerforming the automatic C++11 traversal of the 'int' value in the array 'a'"<<endl<<endl;
     for(auto i : a)
     {

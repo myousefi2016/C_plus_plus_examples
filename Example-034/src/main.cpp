24e3cb95e7ab64a0f5b21bb2b1df4a630d4942ae
David Canino <canino.david@gmail.com>
2016-12-21 02:03:10 +0100

    added the source code, the cmake, and the xcode building infrastructure for the 'Example-034' test

---
+++ Example-034/src/main.cpp
@@ -0,0 +1,53 @@
+/**********************************************************************************************
+ * Examples in the C++ language (the test 'Example-034')
+ *
+ * GitHub repository: http://github.com/davidcanino/C_plus_plus_examples
+ *
+ * Created by David Canino (canino.david@gmail.com), December 2016
+ *
+ * main.cpp - the source file, implementing the main function for the test 'Example-034'.
+ **********************************************************************************************/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+/// This function computes the area of a rectangle with width w and height h.
+int computeArea(int w=20,int h=30)
+{
+	cout<<endl<<"\tThe default parameter 'w' in the 'computeArea()' function: 20"<<endl;
+	cout<<"\tThe default parameter 'h' in the 'computeArea()' function: 30"<<endl;
+	cout<<"\tThe actual parameter 'w' in the 'computeArea()' function: "<<w<<endl;
+	cout<<"\tThe actual parameter 'h' in the 'computeArea()' function: "<<h<<endl;
+	cout.flush();
+	return (w*h);
+}
+
+/// The main function for the <i>Example-034</i> unit test.
+int main(void)
+{
+	int a;
+
+	/* This is the example 'Example-034', where the use of the default parameters in a function is tested! */
+    cout<<endl<<"\tThis is the example 'Example-034' in the C++ language"<<endl<<endl;
+    cout.flush();
+    
+    /* Invoking the 'computeArea()' function by passing no actual parameter */
+    cout<<"\tInvoking the 'computeArea()' function by passing no actual parameter"<<endl;
+	a = computeArea();
+	cout<<"\tThe area of interest: "<<a<<endl<<endl;
+	cout.flush();
+	
+	/* Invoking the 'computeArea()' function by passing only a custom width. */
+    cout<<"\tInvoking the 'computeArea()' function by passing only the custom parameter 'w'=5"<<endl;
+	a = computeArea(5);
+	cout<<"\tThe area of interest: "<<a<<endl<<endl;
+	cout.flush();
+	
+	/* Invoking the 'computeArea()' function by passing both the custom width and the custom height. */
+    cout<<"\tInvoking the 'computeArea()' function by passing both the custom parameter 'w'=5 and the custom parameter 'h'=10"<<endl;
+	a = computeArea(5,10);
+	cout<<"\tThe area of interest: "<<a<<endl<<endl;
+	cout.flush();
+	return EXIT_SUCCESS;
+}

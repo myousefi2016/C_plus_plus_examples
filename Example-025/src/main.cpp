ada66d199d6eddbf8099854951db4e2cc31d2657
David Canino <canino.david@gmail.com>
2017-03-14 00:08:16 +0100

    Minor modifications and updates to several tests

--- Example-025/src/main.cpp
+++ Example-025/src/main.cpp
@@ -1,96 +1,95 @@
-/**********************************************************************************************
- * Examples in the C++ language (the test 'Example-025')
+/**************************************************************************************************
+ * Examples in the C++ language (the 'Example-025' unit test).
  *
  * GitHub repository: http://github.com/davidcanino/C_plus_plus_examples
  *
- * Created by David Canino (canino.david@gmail.com), December 2016
+ * Created by David Canino (canino.david@gmail.com), March 2017
  *
- * main.cpp - the source file, implementing the main function for the test 'Example-025'.
- **********************************************************************************************/
+ * main.cpp - the source file, implementing the main function for the 'Example-025' unit test.
+ **************************************************************************************************/
 
 #include <limits>
 #include <iostream>
 #include <cstdlib>
 using namespace std;
 
-/// The main function for the <i>Example-025</i> unit test.
+/// The main function for the <i>'Example-025'</i> unit test.
 int main(void)
 {
-	/* This is the example 'Example-025', where the properties of the 'char32_t' type are analyzed. */
-    cout<<endl<<"\tThis is the example 'Example-025' in the C++ language"<<endl<<endl;
-    cout.flush();
-	cout<<"\tThe C++ built-in 'char32_t' type:"<<endl<<endl;
-	cout<<"\t-) requires "<<sizeof(char32_t)<<" bytes for being represented"<<endl;
-	if(std::numeric_limits<char32_t>::is_specialized) cout<<"\t-) is specialized"<<endl;
-	else cout<<"\t-) is not specialized"<<endl;
-	if(std::numeric_limits<char32_t>::is_signed) cout<<"\t-) represents signed values"<<endl;
-	else cout<<"\t-) does not represent signed values"<<endl;
-	if(std::numeric_limits<char32_t>::is_integer) cout<<"\t-) represents integer values"<<endl;
-	else cout<<"\t-) does not represent integer values"<<endl;
-	if(std::numeric_limits<char32_t>::is_exact) cout<<"\t-) represents exact values"<<endl;
-	else cout<<"\t-) does not represent exact values"<<endl;
-	if(std::numeric_limits<char32_t>::has_infinity) cout<<"\t-) represents the 'infinity' value as "<<std::numeric_limits<char32_t>::infinity()<<endl;
-	else cout<<"\t-) does not represent the 'infinity' value"<<endl;
-	if(std::numeric_limits<char32_t>::has_quiet_NaN) cout<<"\t-) represents the 'Quiet Not-A-Number' value as "<<std::numeric_limits<char32_t>::quiet_NaN()<<endl;
-	else cout<<"\t-) does not represent the 'Quiet Not-A-Number' value"<<endl;
-	if(std::numeric_limits<char32_t>::has_signaling_NaN) cout<<"\t-) represents the 'Signaling Not-A-Number' value as "<<std::numeric_limits<char32_t>::signaling_NaN()<<endl;
-	else cout<<"\t-) does not represent the 'Signaling Not-A-Number' value"<<endl;
-	if(std::numeric_limits<char32_t>::has_denorm) cout<<"\t-) represents the subnorm values"<<endl;
-	else cout<<"\t-) does not represent the subnorm values"<<endl;
-	if(std::numeric_limits<char32_t>::has_denorm_loss) cout<<"\t-) detects loss of precision when creating a subnormal number as denormalization loss rather than as inexact result"<<endl;
-	else cout<<"\t-) does not detect loss of precision when creating a subnormal number as denormalization loss rather than as inexact result"<<endl;
+	/* This is the 'Example-025' unit test, where the properties of the 'UTF-32 character (char32_t)' C++ built-in type are analyzed. */
+    cout<<endl<<"\tThis is the 'Example-025' unit test in the C++ language."<<endl<<endl;
+	cout<<"\tThe 'UTF-32 character (char32_t)' C++ built-in type:"<<endl<<endl;
+	cout<<"\t-) requires "<<sizeof(char32_t)<<" bytes for being represented;"<<endl;
+	if(std::numeric_limits<char32_t>::is_specialized) cout<<"\t-) is specialized;"<<endl;
+	else cout<<"\t-) is not specialized;"<<endl;
+	if(std::numeric_limits<char32_t>::is_signed) cout<<"\t-) represents signed values;"<<endl;
+	else cout<<"\t-) does not represent signed values;"<<endl;
+	if(std::numeric_limits<char32_t>::is_integer) cout<<"\t-) represents integer values;"<<endl;
+	else cout<<"\t-) does not represent integer values;"<<endl;
+	if(std::numeric_limits<char32_t>::is_exact) cout<<"\t-) represents exact values;"<<endl;
+	else cout<<"\t-) does not represent exact values;"<<endl;
+	if(std::numeric_limits<char32_t>::has_infinity) cout<<"\t-) represents the 'infinity' value as "<<std::numeric_limits<char32_t>::infinity()<<";"<<endl;
+	else cout<<"\t-) does not represent the 'infinity' value;"<<endl;
+	if(std::numeric_limits<char32_t>::has_quiet_NaN) cout<<"\t-) represents the 'Quiet Not-A-Number' value as "<<std::numeric_limits<char32_t>::quiet_NaN()<<";"<<endl;
+	else cout<<"\t-) does not represent the 'Quiet Not-A-Number' value;"<<endl;
+	if(std::numeric_limits<char32_t>::has_signaling_NaN) cout<<"\t-) represents the 'Signaling Not-A-Number' value as "<<std::numeric_limits<char32_t>::signaling_NaN()<<";"<<endl;
+	else cout<<"\t-) does not represent the 'Signaling Not-A-Number' value;"<<endl;
+	if(std::numeric_limits<char32_t>::has_denorm) cout<<"\t-) represents the subnorm values;"<<endl;
+	else cout<<"\t-) does not represent the subnorm values;"<<endl;
+	if(std::numeric_limits<char32_t>::has_denorm_loss) cout<<"\t-) detects loss of precision when creating a subnormal number as denormalization loss rather than as inexact result;"<<endl;
+	else cout<<"\t-) does not detect loss of precision when creating a subnormal number as denormalization loss rather than as inexact result;"<<endl;
 	switch (std::numeric_limits<char32_t>::round_style)
 	{	
 		case round_toward_zero:
 		
-			cout<<"\t-) exploits the 'round toward zero' policy for rounding not completely 'char32_t' values"<<endl;
+			cout<<"\t-) exploits the 'round toward zero' policy for rounding not completely 'char32_t' values;"<<endl;
 			break;
 			
 		case round_to_nearest:
 		
-			cout<<"\t-) exploits the 'round to nearest' policy for rounding not completely 'char32_t' values"<<endl;
+			cout<<"\t-) exploits the 'round to nearest' policy for rounding not completely 'char32_t' values;"<<endl;
 			break;
 			
 		case round_toward_infinity:
 		
-			cout<<"\t-) exploits the 'round to the positive infinity' policy for rounding not completely 'char32_t' values"<<endl;
+			cout<<"\t-) exploits the 'round to the positive infinity' policy for rounding not completely 'char32_t' values;"<<endl;
 			break;
 			
 		case round_toward_neg_infinity:
 		
-			cout<<"\t-) exploits the 'round to the negative infinity' policy for rounding not completely 'char32_t' values"<<endl;
+			cout<<"\t-) exploits the 'round to the negative infinity' policy for rounding not completely 'char32_t' values;"<<endl;
 			break;
 
 		default:
 		
-			cout<<"\t-) does not exploit a policy for rounding not completely 'char32_t' values"<<endl;
+			cout<<"\t-) does not exploit a policy for rounding not completely 'char32_t' values;"<<endl;
 			cout.flush();
 			break;
 	}
 	
 	/* Now, we finalize our analysis! */
-	if(std::numeric_limits<char32_t>::is_iec559) cout<<"\t-) fulfills the requirements of IEC 559 (IEEE 754) standard"<<endl;
-	else cout<<"\t-) does not fulfill the requirements of IEC 559 (IEEE 754) standard"<<endl;
-	if(std::numeric_limits<char32_t>::is_bounded) cout<<"\t-) represents a finit set of values"<<endl;
-	else cout<<"\t-) does not represent a finite set of values"<<endl;
-	if(std::numeric_limits<char32_t>::is_modulo) cout<<"\t-) handles overflows with modulo arithmetic"<<endl;
-	else cout<<"\t-) does not handle overflows with modulo arithmetic"<<endl;
-	cout<<"\t-) requires "<<std::numeric_limits<char32_t>::digits<<" digits"<<endl;
-	cout<<"\t-) requires "<<std::numeric_limits<char32_t>::digits10<<" base-10 digits"<<endl;
-	cout<<"\t-) has "<<std::numeric_limits<char32_t>::radix<<" as the base of the number system for being represented (radix)"<<endl;
-	cout<<"\t-) has "<<std::numeric_limits<char32_t>::min_exponent<<" as one more than the smallest negative power of the radix, such that is a valid normalized floating-point value"<<endl;
-	cout<<"\t-) has "<<std::numeric_limits<char32_t>::min_exponent10<<" as the smallest negative power of 10, such that is a valid normalized floating-point value"<<endl;
-	cout<<"\t-) has "<<std::numeric_limits<char32_t>::max_exponent<<" as one more than the largest integer power of the radix, such that is a valid finite floating-point value"<<endl;
-	cout<<"\t-) has "<<std::numeric_limits<char32_t>::max_exponent10<<" as the largest integer power of 10, such that is a valid normalized floating-point value"<<endl;
-	if(std::numeric_limits<char32_t>::traps) cout<<"\t-) has at least one value that, if used as an argument to an arithmetic operation, will generate a trap"<<endl;
-	else cout<<"\t-) has no value, that, if used as an argument to an arithmetic operation, will generate a trap"<<endl;
-	if(std::numeric_limits<char32_t>::tinyness_before) cout<<"\t-) tests the results of floating-point expressions for underflow before rounding"<<endl;
-	else cout<<"\t-) does not test the results of floating-point expressions for underflow before rounding"<<endl;
-	cout<<"\t-) has "<<(char32_t)std::numeric_limits<char32_t>::min()<<" as the minimum finite value, representable as a 'char32_t' value"<<endl;
-	cout<<"\t-) has "<<(char32_t)std::numeric_limits<char32_t>::min()<<" as the lowest finite value, representable as a 'char32_t' value"<<endl;
-	cout<<"\t-) has "<<(char32_t)std::numeric_limits<char32_t>::max()<<" as the maximum finite value, representable as a 'char32_t' value"<<endl;
-	cout<<"\t-) has "<<(char32_t)std::numeric_limits<char32_t>::epsilon()<<" as the machine epsilon, that is, the difference between 1.0 and the next value, representable as a 'char32_t' value"<<endl;
-	cout<<"\t-) has "<<(char32_t)std::numeric_limits<char32_t>::round_error()<<" as the maximum rounding error"<<endl;
-	cout<<"\t-) has "<<(char32_t)std::numeric_limits<char32_t>::denorm_min()<<" as the minimum positive subnormal value"<<endl<<endl;
+	if(std::numeric_limits<char32_t>::is_iec559) cout<<"\t-) fulfills the requirements of IEC 559 (IEEE 754) standard;"<<endl;
+	else cout<<"\t-) does not fulfill the requirements of IEC 559 (IEEE 754) standard;"<<endl;
+	if(std::numeric_limits<char32_t>::is_bounded) cout<<"\t-) represents a finit set of values;"<<endl;
+	else cout<<"\t-) does not represent a finite set of values;"<<endl;
+	if(std::numeric_limits<char32_t>::is_modulo) cout<<"\t-) handles overflows with modulo arithmetic;"<<endl;
+	else cout<<"\t-) does not handle overflows with modulo arithmetic;"<<endl;
+	cout<<"\t-) requires "<<std::numeric_limits<char32_t>::digits<<" digits;"<<endl;
+	cout<<"\t-) requires "<<std::numeric_limits<char32_t>::digits10<<" base-10 digits;"<<endl;
+	cout<<"\t-) has "<<std::numeric_limits<char32_t>::radix<<" as the base of the number system for being represented (radix);"<<endl;
+	cout<<"\t-) has "<<std::numeric_limits<char32_t>::min_exponent<<" as one more than the smallest negative power of the radix, such that is a valid normalized floating-point value;"<<endl;
+	cout<<"\t-) has "<<std::numeric_limits<char32_t>::min_exponent10<<" as the smallest negative power of 10, such that is a valid normalized floating-point value;"<<endl;
+	cout<<"\t-) has "<<std::numeric_limits<char32_t>::max_exponent<<" as one more than the largest integer power of the radix, such that is a valid finite floating-point value;"<<endl;
+	cout<<"\t-) has "<<std::numeric_limits<char32_t>::max_exponent10<<" as the largest integer power of 10, such that is a valid normalized floating-point value;"<<endl;
+	if(std::numeric_limits<char32_t>::traps) cout<<"\t-) has at least one value that, if used as an argument to an arithmetic operation, will generate a trap;"<<endl;
+	else cout<<"\t-) has no value, that, if used as an argument to an arithmetic operation, will generate a trap;"<<endl;
+	if(std::numeric_limits<char32_t>::tinyness_before) cout<<"\t-) tests the results of floating-point expressions for underflow before rounding;"<<endl;
+	else cout<<"\t-) does not test the results of floating-point expressions for underflow before rounding;"<<endl;
+	cout<<"\t-) has "<<(char32_t)std::numeric_limits<char32_t>::min()<<" as the minimum finite value, representable as a 'char32_t' value;"<<endl;
+	cout<<"\t-) has "<<(char32_t)std::numeric_limits<char32_t>::min()<<" as the lowest finite value, representable as a 'char32_t' value;"<<endl;
+	cout<<"\t-) has "<<(char32_t)std::numeric_limits<char32_t>::max()<<" as the maximum finite value, representable as a 'char32_t' value;"<<endl;
+	cout<<"\t-) has "<<(char32_t)std::numeric_limits<char32_t>::epsilon()<<" as the machine epsilon, that is, the difference between 1.0 and the next value, representable as a 'char32_t' value;"<<endl;
+	cout<<"\t-) has "<<(char32_t)std::numeric_limits<char32_t>::round_error()<<" as the maximum rounding error;"<<endl;
+	cout<<"\t-) has "<<(char32_t)std::numeric_limits<char32_t>::denorm_min()<<" as the minimum positive subnormal value."<<endl<<endl;
 	return EXIT_SUCCESS;
 }
--- Example-029/src/main.cpp
+++ Example-029/src/main.cpp
@@ -1,96 +1,96 @@
-/**********************************************************************************************
- * Examples in the C++ language (the test 'Example-029')
+/**************************************************************************************************
+ * Examples in the C++ language (the 'Example-029' unit test).
  *
  * GitHub repository: http://github.com/davidcanino/C_plus_plus_examples
  *
- * Created by David Canino (canino.david@gmail.com), December 2016
+ * Created by David Canino (canino.david@gmail.com), March 2017
  *
- * main.cpp - the source file, implementing the main function for the test 'Example-029'.
- **********************************************************************************************/
+ * main.cpp - the source file, implementing the main function for the 'Example-029' unit test.
+ **************************************************************************************************/
 
 #include <limits>
 #include <iostream>
 #include <cstdlib>
 using namespace std;
 
-/// The main function for the <i>Example-029</i> unit test.
+/// The main function for the <i>'Example-029'</i> unit test.
 int main(void)
 {
-	/* This is the example 'Example-029', where the properties of the 'unsigned long long int' type are analyzed. */
-    cout<<endl<<"\tThis is the example 'Example-029' in the C++ language"<<endl<<endl;
+	/* This is the 'Example-029' unit test, where the properties of the 'unsigned character (uchar)' C++ built-in type are analyzed. */
+    cout<<endl<<"\tThis is the 'Example-029' unit test in the C++ language."<<endl<<endl;
     cout.flush();
-	cout<<"\tThe C++ built-in 'unsigned long long int' type:"<<endl<<endl;
-	cout<<"\t-) requires "<<sizeof(unsigned long long int)<<" byte for being represented"<<endl;
-	if(std::numeric_limits<unsigned long long int>::is_specialized) cout<<"\t-) is specialized"<<endl;
-	else cout<<"\t-) is not specialized"<<endl;
-	if(std::numeric_limits<unsigned long long int>::is_signed) cout<<"\t-) represents signed values"<<endl;
-	else cout<<"\t-) does not represent signed values"<<endl;
-	if(std::numeric_limits<unsigned long long int>::is_integer) cout<<"\t-) represents integer values"<<endl;
-	else cout<<"\t-) does not represent integer values"<<endl;
-	if(std::numeric_limits<unsigned long long int>::is_exact) cout<<"\t-) represents exact values"<<endl;
-	else cout<<"\t-) does not represent exact values"<<endl;
-	if(std::numeric_limits<unsigned long long int>::has_infinity) cout<<"\t-) represents the 'infinity' value as "<<std::numeric_limits<unsigned long long int>::infinity()<<endl;
-	else cout<<"\t-) does not represent the 'infinity' value"<<endl;
-	if(std::numeric_limits<unsigned long long int>::has_quiet_NaN) cout<<"\t-) represents the 'Quiet Not-A-Number' value as "<<std::numeric_limits<unsigned long long int>::quiet_NaN()<<endl;
-	else cout<<"\t-) does not represent the 'Quiet Not-A-Number' value"<<endl;
-	if(std::numeric_limits<unsigned long long int>::has_signaling_NaN) cout<<"\t-) represents the 'Signaling Not-A-Number' value as "<<std::numeric_limits<unsigned long long int>::signaling_NaN()<<endl;
-	else cout<<"\t-) does not represent the 'Signaling Not-A-Number' value"<<endl;
-	if(std::numeric_limits<unsigned long long int>::has_denorm) cout<<"\t-) represents the subnorm values"<<endl;
-	else cout<<"\t-) does not represent the subnorm values"<<endl;
-	if(std::numeric_limits<unsigned long long int>::has_denorm_loss) cout<<"\t-) detects loss of precision when creating a subnormal number as denormalization loss rather than as inexact result"<<endl;
-	else cout<<"\t-) does not detect loss of precision when creating a subnormal number as denormalization loss rather than as inexact result"<<endl;
+	cout<<"\tThe 'unsigned long long integer (unsigned long long int)' C++ built-in type:"<<endl<<endl;
+	cout<<"\t-) requires "<<sizeof(unsigned long long int)<<" byte for being represented;"<<endl;
+	if(std::numeric_limits<unsigned long long int>::is_specialized) cout<<"\t-) is specialized;"<<endl;
+	else cout<<"\t-) is not specialized;"<<endl;
+	if(std::numeric_limits<unsigned long long int>::is_signed) cout<<"\t-) represents signed values;"<<endl;
+	else cout<<"\t-) does not represent signed values;"<<endl;
+	if(std::numeric_limits<unsigned long long int>::is_integer) cout<<"\t-) represents integer values;"<<endl;
+	else cout<<"\t-) does not represent integer values;"<<endl;
+	if(std::numeric_limits<unsigned long long int>::is_exact) cout<<"\t-) represents exact values;"<<endl;
+	else cout<<"\t-) does not represent exact values;"<<endl;
+	if(std::numeric_limits<unsigned long long int>::has_infinity) cout<<"\t-) represents the 'infinity' value as "<<std::numeric_limits<unsigned long long int>::infinity()<<";"<<endl;
+	else cout<<"\t-) does not represent the 'infinity' value;"<<endl;
+	if(std::numeric_limits<unsigned long long int>::has_quiet_NaN) cout<<"\t-) represents the 'Quiet Not-A-Number' value as "<<std::numeric_limits<unsigned long long int>::quiet_NaN()<<";"<<endl;
+	else cout<<"\t-) does not represent the 'Quiet Not-A-Number' value;"<<endl;
+	if(std::numeric_limits<unsigned long long int>::has_signaling_NaN) cout<<"\t-) represents the 'Signaling Not-A-Number' value as "<<std::numeric_limits<unsigned long long int>::signaling_NaN()<<";"<<endl;
+	else cout<<"\t-) does not represent the 'Signaling Not-A-Number' value;"<<endl;
+	if(std::numeric_limits<unsigned long long int>::has_denorm) cout<<"\t-) represents the subnorm values;"<<endl;
+	else cout<<"\t-) does not represent the subnorm values;"<<endl;
+	if(std::numeric_limits<unsigned long long int>::has_denorm_loss) cout<<"\t-) detects loss of precision when creating a subnormal number as denormalization loss rather than as inexact result;"<<endl;
+	else cout<<"\t-) does not detect loss of precision when creating a subnormal number as denormalization loss rather than as inexact result;"<<endl;
 	switch (std::numeric_limits<unsigned long long int>::round_style)
 	{	
 		case round_toward_zero:
 		
-			cout<<"\t-) exploits the 'round toward zero' policy for rounding not completely 'unsigned long long int' values"<<endl;
+			cout<<"\t-) exploits the 'round toward zero' policy for rounding not completely 'unsigned long long int' values;"<<endl;
 			break;
 			
 		case round_to_nearest:
 		
-			cout<<"\t-) exploits the 'round to nearest' policy for rounding not completely 'unsigned long long int' values"<<endl;
+			cout<<"\t-) exploits the 'round to nearest' policy for rounding not completely 'unsigned long long int' values;"<<endl;
 			break;
 			
 		case round_toward_infinity:
 		
-			cout<<"\t-) exploits the 'round to the positive infinity' policy for rounding not completely 'unsigned long long int' values"<<endl;
+			cout<<"\t-) exploits the 'round to the positive infinity' policy for rounding not completely 'unsigned long long int' values;"<<endl;
 			break;
 			
 		case round_toward_neg_infinity:
 		
-			cout<<"\t-) exploits the 'round to the negative infinity' policy for rounding not completely 'unsigned long long int' values"<<endl;
+			cout<<"\t-) exploits the 'round to the negative infinity' policy for rounding not completely 'unsigned long long int' values;"<<endl;
 			break;
 
 		default:
 		
-			cout<<"\t-) does not exploit a policy for rounding not completely 'unsigned long long int' values"<<endl;
+			cout<<"\t-) does not exploit a policy for rounding not completely 'unsigned long long int' values;"<<endl;
 			cout.flush();
 			break;
 	}
 	
 	/* Now, we finalize our analysis! */
-	if(std::numeric_limits<unsigned long long int>::is_iec559) cout<<"\t-) fulfills the requirements of IEC 559 (IEEE 754) standard"<<endl;
-	else cout<<"\t-) does not fulfill the requirements of IEC 559 (IEEE 754) standard"<<endl;
-	if(std::numeric_limits<unsigned long long int>::is_bounded) cout<<"\t-) represents a finit set of values"<<endl;
-	else cout<<"\t-) does not represent a finite set of values"<<endl;
-	if(std::numeric_limits<unsigned long long int>::is_modulo) cout<<"\t-) handles overflows with modulo arithmetic"<<endl;
-	else cout<<"\t-) does not handle overflows with modulo arithmetic"<<endl;
-	cout<<"\t-) requires "<<std::numeric_limits<unsigned long long int>::digits<<" digits"<<endl;
-	cout<<"\t-) requires "<<std::numeric_limits<unsigned long long int>::digits10<<" base-10 digits"<<endl;
-	cout<<"\t-) has "<<std::numeric_limits<unsigned long long int>::radix<<" as the base of the number system for being represented (radix)"<<endl;
-	cout<<"\t-) has "<<std::numeric_limits<unsigned long long int>::min_exponent<<" as one more than the smallest negative power of the radix, such that is a valid normalized floating-point value"<<endl;
-	cout<<"\t-) has "<<std::numeric_limits<unsigned long long int>::min_exponent10<<" as the smallest negative power of 10, such that is a valid normalized floating-point value"<<endl;
-	cout<<"\t-) has "<<std::numeric_limits<unsigned long long int>::max_exponent<<" as one more than the largest integer power of the radix, such that is a valid finite floating-point value"<<endl;
-	cout<<"\t-) has "<<std::numeric_limits<unsigned long long int>::max_exponent10<<" as the largest integer power of 10, such that is a valid normalized floating-point value"<<endl;
-	if(std::numeric_limits<unsigned long long int>::traps) cout<<"\t-) has at least one value that, if used as an argument to an arithmetic operation, will generate a trap"<<endl;
-	else cout<<"\t-) has no value, that, if used as an argument to an arithmetic operation, will generate a trap"<<endl;
-	if(std::numeric_limits<unsigned long long int>::tinyness_before) cout<<"\t-) tests the results of floating-point expressions for underflow before rounding"<<endl;
-	else cout<<"\t-) does not test the results of floating-point expressions for underflow before rounding"<<endl;
-	cout<<"\t-) has "<<(unsigned long long int)std::numeric_limits<unsigned long long int>::min()<<" as the minimum finite value, representable as an 'unsigned long long int' value"<<endl;
-	cout<<"\t-) has "<<(unsigned long long int)std::numeric_limits<unsigned long long int>::min()<<" as the lowest finite value, representable as an 'unsigned long long int' value"<<endl;
-	cout<<"\t-) has "<<(unsigned long long int)std::numeric_limits<unsigned long long int>::max()<<" as the maximum finite value, representable as an 'unsigned long long int' value"<<endl;
-	cout<<"\t-) has "<<(unsigned long long int)std::numeric_limits<unsigned long long int>::epsilon()<<" as the machine epsilon, that is, the difference between 1.0 and the next value, representable as an 'unsigned long long int' value"<<endl;
-	cout<<"\t-) has "<<(unsigned long long int)std::numeric_limits<unsigned long long int>::round_error()<<" as the maximum rounding error"<<endl;
-	cout<<"\t-) has "<<(unsigned long long int)std::numeric_limits<unsigned long long int>::denorm_min()<<" as the minimum positive subnormal value"<<endl<<endl;
+	if(std::numeric_limits<unsigned long long int>::is_iec559) cout<<"\t-) fulfills the requirements of IEC 559 (IEEE 754) standard;"<<endl;
+	else cout<<"\t-) does not fulfill the requirements of IEC 559 (IEEE 754) standard;"<<endl;
+	if(std::numeric_limits<unsigned long long int>::is_bounded) cout<<"\t-) represents a finit set of values;"<<endl;
+	else cout<<"\t-) does not represent a finite set of values;"<<endl;
+	if(std::numeric_limits<unsigned long long int>::is_modulo) cout<<"\t-) handles overflows with modulo arithmetic;"<<endl;
+	else cout<<"\t-) does not handle overflows with modulo arithmetic;"<<endl;
+	cout<<"\t-) requires "<<std::numeric_limits<unsigned long long int>::digits<<" digits;"<<endl;
+	cout<<"\t-) requires "<<std::numeric_limits<unsigned long long int>::digits10<<" base-10 digits;"<<endl;
+	cout<<"\t-) has "<<std::numeric_limits<unsigned long long int>::radix<<" as the base of the number system for being represented (radix);"<<endl;
+	cout<<"\t-) has "<<std::numeric_limits<unsigned long long int>::min_exponent<<" as one more than the smallest negative power of the radix, such that is a valid normalized floating-point value;"<<endl;
+	cout<<"\t-) has "<<std::numeric_limits<unsigned long long int>::min_exponent10<<" as the smallest negative power of 10, such that is a valid normalized floating-point value;"<<endl;
+	cout<<"\t-) has "<<std::numeric_limits<unsigned long long int>::max_exponent<<" as one more than the largest integer power of the radix, such that is a valid finite floating-point value;"<<endl;
+	cout<<"\t-) has "<<std::numeric_limits<unsigned long long int>::max_exponent10<<" as the largest integer power of 10, such that is a valid normalized floating-point value;"<<endl;
+	if(std::numeric_limits<unsigned long long int>::traps) cout<<"\t-) has at least one value that, if used as an argument to an arithmetic operation, will generate a trap;"<<endl;
+	else cout<<"\t-) has no value, that, if used as an argument to an arithmetic operation, will generate a trap;"<<endl;
+	if(std::numeric_limits<unsigned long long int>::tinyness_before) cout<<"\t-) tests the results of floating-point expressions for underflow before rounding;"<<endl;
+	else cout<<"\t-) does not test the results of floating-point expressions for underflow before rounding;"<<endl;
+	cout<<"\t-) has "<<(unsigned long long int)std::numeric_limits<unsigned long long int>::min()<<" as the minimum finite value, representable as an 'unsigned long long int' value;"<<endl;
+	cout<<"\t-) has "<<(unsigned long long int)std::numeric_limits<unsigned long long int>::min()<<" as the lowest finite value, representable as an 'unsigned long long int' value;"<<endl;
+	cout<<"\t-) has "<<(unsigned long long int)std::numeric_limits<unsigned long long int>::max()<<" as the maximum finite value, representable as an 'unsigned long long int' value;"<<endl;
+	cout<<"\t-) has "<<(unsigned long long int)std::numeric_limits<unsigned long long int>::epsilon()<<" as the machine epsilon, that is, the difference between 1.0 and the next value, representable as an 'unsigned long long int' value;"<<endl;
+	cout<<"\t-) has "<<(unsigned long long int)std::numeric_limits<unsigned long long int>::round_error()<<" as the maximum rounding error;"<<endl;
+	cout<<"\t-) has "<<(unsigned long long int)std::numeric_limits<unsigned long long int>::denorm_min()<<" as the minimum positive subnormal value."<<endl<<endl;
 	return EXIT_SUCCESS;
 }

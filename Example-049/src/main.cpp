c6c09abddb5e13fa2dbb8c7c814a1ca123e896b9
David Canino <canino.david@gmail.com>
2017-03-14 22:45:30 +0100

    Minor modifications and updates to several tests

--- Example-049/src/main.cpp
+++ Example-049/src/main.cpp
@@ -1,13 +1,13 @@
-/**********************************************************************************************
- * Examples in the C++ language (the test 'Example-049')
+/**************************************************************************************************
+ * Examples in the C++ language (the 'Example-049' unit test).
  *
  * GitHub repository: http://github.com/davidcanino/C_plus_plus_examples
  *
- * Created by David Canino (canino.david@gmail.com), January 2017
+ * Created by David Canino (canino.david@gmail.com), March 2017
  *
- * main.cpp - the source file, implementing the main function for the test 'Example-049'.
- **********************************************************************************************/
-
+ * main.cpp - the source file, implementing the main function for the 'Example-049' unit test.
+ **************************************************************************************************/
+ 
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -15,9 +15,16 @@
 using namespace std;
 
 /// This function exports the content of an array (containing several 'int' values) on a generic output stream.
-void print( int* val,unsigned int n,ostream &os=cout)
+void print(int* val,unsigned int n,ostream &os=cout)
 {
-	for(unsigned int k=0;k<n;k++) { os<<"\tElement in position #"<<k<<": "<<*(val+k)<<endl; }
+	for(unsigned int k=0;k<n;k++) 
+	{ 
+		os<<"\tthe element in position #"<<k<<" is "<<*(val+k);
+		if(k<n-1) os<<";"<<endl;
+		else os<<"."<<endl; 
+	}
+	
+	/* We add EOL ... */
 	os<<endl;
 	os.flush();
 }
@@ -25,7 +32,14 @@ void print( int* val,unsigned int n,ostream &os=cout)
 /// This function exports the content of an array (containing several 'string' values) on a generic output stream.
 void print(string* val,unsigned int n,ostream &os=cout)
 {
-	for(unsigned int k=0;k<n;k++) { os<<"\tElement in position #"<<k<<": "<<"'"<<*(val+k)<<"'"<<endl; }
+	for(unsigned int k=0;k<n;k++) 
+	{ 
+		os<<"\tThe element in position #"<<k<<" is "<<*(val+k);
+		if(k<n-1) os<<";"<<endl;
+		else os<<"."<<endl; 
+	}
+	
+	/* We add EOL ... */
 	os<<endl;
 	os.flush();
 }
@@ -36,35 +50,34 @@ int main(void)
 	int numbers[10];
 	string seasons[4];
 
-	/* This is the example 'Example-049', where the use of the arrays is tested! */
-    cout<<endl<<"\tThis is the example 'Example-049' in the C++ language"<<endl<<endl;
+	/* This is the 'Example-049' unit test, where the use of the arrays (described by the C-like pointers) is tested! */
+    cout<<endl<<"\tThis is the 'Example-049' unit test in the C++ language."<<endl<<endl;
     cout.flush();
-
-	/* Generating 10 pseudo-random values, and saving these pseudo-random values into an array. */
+    
+    /* TASK #1 - generating 10 pseudo-random values, and saving these pseudo-random values into an array (described by a C-like pointer). */
 	srand(time(0));
-	cout<<"\tGenerating 10 pseudo-random integer values value to be saved into an array ... ";
+	cout<<"\tGenerating 10 pseudo-random integer values value to be saved into an array (described by a C-like pointer) ... ";
 	cout.flush();
 	for(unsigned int k=0;k<10;k++) numbers[k]=rand();
 	cout<<"ok"<<endl<<endl;
 	cout.flush();
 	print(numbers,10,cout);
 	
-	/* Generating the array 'seasons', containing the names of all seasons! */
-	cout<<"\tInitializing an array, containing the names of all seasons ... ";
+	/* TASK #2 - generating the array 'seasons', containing the names of all seasons! */
+	cout<<"\tInitializing an array (described by a C-like pointer), containing the names of all seasons ... ";
 	seasons[0] = "summer";
 	seasons[1] = "fall";
 	seasons[2] = "winter";
 	seasons[3] = "spring";
 	cout<<"ok"<<endl<<endl;
 	cout.flush();
-	cout<<"\tPointer to the array of interest: "<<seasons<<endl;
+	cout<<"\tThe C-like pointer to the array of interest is "<<seasons<<"."<<endl;
 	print(seasons,4);
 	
-	/* Automatic initialization for an array, containing the names for the week days! */
-	cout<<"\tInitializing automatically an array, containing the names for the week days ... ";
+	/* TASK #3 - automatic initialization for an array, containing the names for the week days! */
+	cout<<"\tInitializing automatically an array (described by a C-like pointer), containing the names for the week days ... ";
     string weekdays[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
     cout<<"ok"<<endl<<endl;
     print(weekdays,7);
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
-
--- Example-063/src/main.cpp
+++ Example-063/src/main.cpp
@@ -1,13 +1,13 @@
-/**********************************************************************************************
- * Examples in the C++ language (the test 'Example-063')
+/**************************************************************************************************
+ * Examples in the C++ language (the 'Example-063' unit test).
  *
  * GitHub repository: http://github.com/davidcanino/C_plus_plus_examples
  *
  * Created by David Canino (canino.david@gmail.com), March 2017
  *
- * main.cpp - the source file, implementing the main function for the test 'Example-063'.
- **********************************************************************************************/
-
+ * main.cpp - the source file, implementing the main function for the 'Example-063' unit test.
+ **************************************************************************************************/
+ 
 #include <cstdlib>
 #include <string>
 #include <iostream>
@@ -24,19 +24,21 @@ using namespace std;
 /// This function is a custom function for comparing the 'string' keys.
 bool compareStrings(const string& a,const string& b) { return (a<b); }
 
-/// The main function for the <i>Example-063</i> unit test.
+/// The main function for the <i>'Example-063'</i> unit test.
 int main(void)
 {
-	/* This is the example 'Example-063', where the 'tuple' containers are validated. */
-    cout<<endl<<"\tThis is the example 'Example-063' in the C++ language"<<endl<<endl;
+	uint n=0;
+
+	/* This is the 'Example-063' unit test, where the 'map' containers are validated. */
+    cout<<endl<<"\tThis is the 'Example-063' unit test in the C++ language."<<endl<<endl;
     cout.flush();
     
     /* TASK #1 - Creating an empty map 'm0', containing no association. New associations will be added separately to the map 'm0'. */
     cout<<"\tCreating a new map 'm0' for storing associations between a 'string' and an 'uint' value ... ";
     map<string,uint> m0;
     cout<<"ok"<<endl;
-    if(m0.empty()==true) cout<<"\tThe new map 'm0' contains no association between a 'string' and an 'uint' value"<<endl;
-    else cout<<"\tThe new map 'm0' contains "<<m0.size()<<" associations between a 'string' and an 'uint' value"<<endl;
+    if(m0.empty()==true) cout<<"\tThe new map 'm0' contains no association between a 'string' and an 'uint' value."<<endl;
+    else cout<<"\tThe new map 'm0' contains "<<m0.size()<<" associations between a 'string' and an 'uint' value."<<endl;
     cout<<"\tAdding several associations between a 'string' and an 'uint' value ... ";
     m0["One"]=(uint)1;
     m0["Two"]=(uint)2;
@@ -45,84 +47,97 @@ int main(void)
     m0.insert(std::pair<string,uint>("Five",5));
     m0.insert(m0.begin(),std::pair<string,uint>("Four",4));
     cout<<"ok"<<endl;
-    if(m0.empty()==true) cout<<"\tThe new map 'm0' contains no association between a 'string' and an 'uint' value"<<endl;
+ 	if(m0.empty()==true) cout<<"\tThe new map 'm0' contains no association between a 'string' and an 'uint' value."<<endl;
     else
     {
-    	cout<<"\tThe new map 'm0' contains "<<m0.size()<<" associations between a 'string' and an 'uint' value, enumerated in the forward direction:"<<endl<<endl;
-    	for(auto it=m0.cbegin();it!=m0.cend();it++) { cout<<"\tThe string '"<<it->first<<"' is associated with the 'uint' value "<<it->second<<endl; }
+    	/* We export the associations in the not empty map 'm0'. */
+    	cout<<"\tThe new map 'm0' contains the following "<<m0.size()<<" associations between a 'string' and an 'uint' value, enumerated in the forward direction:"<<endl<<endl;
+    	for(auto it=m0.cbegin();it!=m0.cend();it++,n++) 
+    	{ 
+    		cout<<"\t\tThe string '"<<it->first<<"' is associated with the 'uint' value "<<it->second; 
+    		if(n==m0.size()-1) cout<<".";
+    		else cout<<";";
+    		cout<<endl; 
+    	}
+    	
+  		/* We export EOL... */
     	cout<<endl;
     	cout.flush();
     }
-    
-    /* TASK #2 - Creating a copy of another map. */
+ 	
+ 	/* TASK #2 - Creating a copy of another map. */
     cout<<"\tCreating the copy 'm1' of the existing map 'm0' ... ";
     map<string,uint> m1(m0.crbegin(),m0.crend());
     cout<<"ok"<<endl;
-    if(m1.empty()==true) cout<<"\tThe new map 'm1' contains no association between a 'string' and an 'uint' value"<<endl;
-    else cout<<"\tThe new map 'm1' contains "<<m1.size()<<" associations between a 'string' and an 'uint' value"<<endl;
-    if(m0==m1) cout<<"\tThe maps 'm0' and 'm1' contain exactly the same associations between a 'string' and an 'uint' value (with respect to the '==' operator)"<<endl;
-    else cout<<"\tThe maps 'm0' and 'm1' do not contain exactly the same associations between a 'string' and an 'uint' value (with respect to the '==' operator)"<<endl;
-    cout<<"\tThe maximum capacity of the map 'm1': "<<m1.max_size()<<endl;
-    if(m1.count("Three")==0) { cout<<"\tThe map 'm1' contains no association, involving the string 'Three'"<<endl; }
-    else { cout<<"\tThe map 'm1' contains an association, involving the string 'Three'"<<endl; }
+    if(m1.empty()==true) cout<<"\tThe new map 'm1' contains no association between a 'string' and an 'uint' value."<<endl;
+    else cout<<"\tThe new map 'm1' contains "<<m1.size()<<" associations between a 'string' and an 'uint' value."<<endl;
+ 	if(m0==m1) cout<<"\tThe maps 'm0' and 'm1' contain exactly the same associations between a 'string' and an 'uint' value (with respect to the '==' operator)."<<endl;
+    else cout<<"\tThe maps 'm0' and 'm1' do not contain exactly the same associations between a 'string' and an 'uint' value (with respect to the '==' operator)."<<endl;
+    cout<<"\tThe maximum capacity of the map 'm1' is "<<m1.max_size()<<"."<<endl;
+    if(m1.count("Three")==0) { cout<<"\tThe map 'm1' contains no association, involving the string 'Three'."<<endl; }
+    else { cout<<"\tThe map 'm1' contains an association, involving the string 'Three'."<<endl; }
     cout<<"\tRetrieving the lower bound of the 'Five' string key ... ";
     auto itl=m1.lower_bound("Five");
-    cout<<"ok. Found the string '"<<itl->first<<"', associated with the 'uint' value "<<itl->second<<endl;
+    cout<<"ok. Found the string '"<<itl->first<<"', associated with the 'uint' value "<<itl->second<<"."<<endl;
     cout<<"\tRetrieving the upper bound of the 'One' string key ... ";
     auto itu=m1.upper_bound("Five");
-    cout<<"ok. Found the string '"<<itu->first<<"', associated with the 'uint' value "<<itu->second<<endl;
+    cout<<"ok. Found the string '"<<itu->first<<"', associated with the 'uint' value "<<itu->second<<"."<<endl;
     cout<<endl;
     cout.flush();
-
-    /* TASK #3 - Another method for creating a copy of a map. */
+    
+    /* TASK #3 - Creating again a copy of another map. */
     cout<<"\tCreating the copy 'm2' of the existing map 'm1' ... ";
     auto m2=m1;
     cout<<"ok"<<endl;
-    if(m2.empty()==true) cout<<"\tThe new map 'm2' contains no association between a 'string' and an 'uint' value"<<endl;
-    else cout<<"\tThe new map 'm2' contains "<<m2.size()<<" associations between a 'string' and an 'uint' value"<<endl;
-    if(m1==m2) cout<<"\tThe maps 'm1' and 'm2' contain exactly the same associations between a 'string' and an 'uint' value (with respect to the '==' operator)"<<endl;
-    else cout<<"\tThe maps 'm1' and 'm2' do not contain exactly the same associations between a 'string' and an 'uint' value (with respect to the '==' operator)"<<endl;
-    cout<<"\tThe map 'm2' contains the following associations between a 'string' and an 'uint' value:"<<endl<<endl;
-    for(auto it=m2.begin();it!=m2.end();it++) { cout<<"\tThe string '"<<it->first<<"' is associated with the 'uint' value "<<it->second<<endl; }
-    cout<<endl;
+    if(m1==m2) cout<<"\tThe maps 'm1' and 'm2' contain exactly the same associations between a 'string' and an 'uint' value (with respect to the '==' operator)."<<endl;
+    else cout<<"\tThe maps 'm1' and 'm2' do not contain exactly the same associations between a 'string' and an 'uint' value (with respect to the '==' operator)."<<endl;
     auto it=m2.find("Four");
     if(it!=m2.end())
     {
-    	cout<<"\tThe map 'm2' contains an association, involving the string 'Four'"<<endl;
+    	cout<<"\tThe map 'm2' contains an association, involving the string 'Four'."<<endl;
     	cout<<"\tRemoving the association, involving the string 'Four', from the map 'm2' ... ";
     	m2.erase(it);
     	cout<<"ok"<<endl;
-    	cout<<"\tThe new map 'm2' contains the following "<<m2.size()<<" associations between a 'string' and an 'uint' value:"<<endl;
-    	for(auto it=m2.rbegin();it!=m2.rend();it++) { cout<<"\tThe string '"<<it->first<<"' is associated with the 'uint' value "<<it->second<<endl; }
-		cout<<endl;
+    	cout<<"\tThe new map 'm2' contains the following "<<m2.size()<<" associations between a 'string' and an 'uint' value:"<<endl<<endl;
+    	n=0;
+    	for(auto it=m2.cbegin();it!=m2.cend();it++,n++) 
+    	{ 
+    		cout<<"\t\tThe string '"<<it->first<<"' is associated with the 'uint' value "<<it->second; 
+    		if(n==m2.size()-1) cout<<".";
+    		else cout<<";";
+    		cout<<endl; 
+    	}
+    	
+  		/* We export EOL... */
+    	cout<<endl;
+    	cout.flush();
     }
     else cout<<"\tThe map 'm2' contains no association, involving the string 'Two'"<<endl;
-    if(m1==m2) cout<<"\tThe maps 'm1' and 'm2' contain exactly the same associations between a 'string' and an 'uint' value (with respect to the '==' operator)"<<endl;
-    else cout<<"\tThe maps 'm1' and 'm2' do not contain exactly the same associations between a 'string' and an 'uint' value (with respect to the '==' operator)"<<endl;
-    if(m1!=m2) cout<<"\tThe maps 'm1' and 'm2' do not contain exactly the same associations between a 'string' and an 'uint' value (with respect to the '!=' operator)"<<endl;
-    else cout<<"\tThe maps 'm1' and 'm2' contain exactly the same associations between a 'string' and an 'uint' value (with respect to the '!=' operator)"<<endl;
+    if(m1==m2) cout<<"\tThe maps 'm1' and 'm2' contain exactly the same associations between a 'string' and an 'uint' value (with respect to the '==' operator)."<<endl;
+    else cout<<"\tThe maps 'm1' and 'm2' do not contain exactly the same associations between a 'string' and an 'uint' value (with respect to the '==' operator)."<<endl;
     cout<<endl;
+    cout.flush();
     
-    /* TASK #4 - Creating a new map 'm3' for storing the swapping operation. */
+    /* TASK #4 - Creating a new map 'm3' for performing the swapping operation. */
     cout<<"\tCreating an empty map 'm3' for storing associations between a 'string' and an 'uint' value ... ";
     map<string,uint> m3;
     cout<<"ok"<<endl;
-    if(m3.empty()==true) cout<<"\tThe new map 'm3' contains no association between a 'string' and an 'uint' value"<<endl;
-    else cout<<"\tThe new map 'm3' contains "<<m3.size()<<" associations between a 'string' and an 'uint' value"<<endl;
+    if(m3.empty()==true) cout<<"\tThe new map 'm3' contains no association between a 'string' and an 'uint' value."<<endl;
+    else cout<<"\tThe new map 'm3' contains "<<m3.size()<<" associations between a 'string' and an 'uint' value."<<endl;
     if(m1.empty()==true) cout<<"\tThe new map 'm1' contains no association between a 'string' and an 'uint' value"<<endl;
     else cout<<"\tThe new map 'm1' contains "<<m1.size()<<" associations between a 'string' and an 'uint' value"<<endl;
     cout<<"\tSwapping the content of the maps 'm1' and 'm3' ... ";
     m1.swap(m3);
     cout<<"ok"<<endl;
-    if(m3.empty()==true) cout<<"\tThe new map 'm3' contains no association between a 'string' and an 'uint' value"<<endl;
-    else cout<<"\tThe new map 'm3' contains "<<m3.size()<<" associations between a 'string' and an 'uint' value"<<endl;
-    if(m1.empty()==true) cout<<"\tThe new map 'm1' contains no association between a 'string' and an 'uint' value"<<endl;
-    else cout<<"\tThe new map 'm1' contains "<<m1.size()<<" associations between a 'string' and an 'uint' value"<<endl;
+    if(m3.empty()==true) cout<<"\tThe new map 'm3' contains no association between a 'string' and an 'uint' value."<<endl;
+    else cout<<"\tThe new map 'm3' contains "<<m3.size()<<" associations between a 'string' and an 'uint' value."<<endl;
+    if(m1.empty()==true) cout<<"\tThe new map 'm1' contains no association between a 'string' and an 'uint' value."<<endl;
+    else cout<<"\tThe new map 'm1' contains "<<m1.size()<<" associations between a 'string' and an 'uint' value."<<endl;
     cout<<endl;
     cout.flush();
     
-    /* TASK #5 - Allocating a new map 'm4' by using a custom function for sorting the keys. */
-    cout<<"\tAllocating a new map 'm4' for storing associations between a 'string' and an 'uint' value by using the custom function 'compareStrings()' for sorting correctly the keys ... ";
+     /* TASK #5 - Allocating a new map 'm4' by using a custom function for sorting the keys. */
+    cout<<"\tAllocating a new map 'm4' for storing associations between a 'string' and an 'uint' value by using the custom function 'compareStrings()' for sorting correctly the keys ('strings', in this case) ... ";
     map< string, uint, bool(*)(const string&,const string&)> *m4=nullptr;
     m4=new map< string, uint, bool(*)(const string&,const string&)>(compareStrings);
     cout<<"ok"<<endl;
@@ -133,53 +148,61 @@ int main(void)
     m4->emplace(string("One"),1);
     m4->emplace_hint(m4->end(),string("Ten"),10);
     cout<<"ok"<<endl;
-    if(m4->empty()==true) cout<<"\tThe new map 'm4' contains no association between a 'string' and an 'uint' value"<<endl;
-    else cout<<"\tThe new map 'm4' contains "<<m4->size()<<" associations between a 'string' and an 'uint' value"<<endl;
-    cout<<"\tThe string 'Thirteen' is associated with the 'uint' value "<<m4->at(string("Thirteen"))<<endl;
-    cout<<"\tThe string 'Fourteen' is associated with the 'uint' value "<<m4->at(string("Fourteen"))<<endl;
-    cout<<"\tThe string 'Nine' is associated with the 'uint' value "<<m4->at(string("Nine"))<<endl;
-    cout<<"\tThe string 'One' is associated with the 'uint' value "<<m4->at(string("One"))<<endl;
-    cout<<"\tThe string 'Ten' is associated with the 'uint' value "<<m4->at(string("Ten"))<<endl<<endl;
+    if(m4->empty()==true) cout<<"\tThe new map 'm4' contains no association between a 'string' and an 'uint' value."<<endl;
+    else cout<<"\tThe new map 'm4' contains the following "<<m4->size()<<" associations between a 'string' and an 'uint' value."<<endl<<endl;
+    cout<<"\t\tThe string 'Thirteen' is associated with the 'uint' value "<<m4->at(string("Thirteen"))<<";"<<endl;
+    cout<<"\t\tThe string 'Fourteen' is associated with the 'uint' value "<<m4->at(string("Fourteen"))<<";"<<endl;
+    cout<<"\t\tThe string 'Nine' is associated with the 'uint' value "<<m4->at(string("Nine"))<<";"<<endl;
+    cout<<"\t\tThe string 'One' is associated with the 'uint' value "<<m4->at(string("One"))<<";"<<endl;
+    cout<<"\t\tThe string 'Ten' is associated with the 'uint' value "<<m4->at(string("Ten"))<<"."<<endl<<endl;
+    cout.flush();
     
     /* TASK #6 - allocating a new C-like array by using the allocator of the map 'm4' */
     cout<<"\tAllocating a new C-like array of 6 locations (containing a pair 'string'/'uint') by using the allocator of the map 'm4' ... ";
     std::pair<const string,uint>* p = m4->get_allocator().allocate(6);
-    cout<<"ok"<<endl<<"\tInitializing the locations of the new C-like array ... ";
+    cout<<"ok"<<endl<<"\tInitializing the locations of the new C-like array (stored as pairs) ... ";
     m4->get_allocator().construct(&p[0],make_pair<const string,uint>(string("Zero"),0));
     m4->get_allocator().construct(&p[1],make_pair<const string,uint>(string("One"),1));
     m4->get_allocator().construct(&p[2],make_pair<const string,uint>(string("Two"),2));
     m4->get_allocator().construct(&p[3],make_pair<const string,uint>(string("Three"),3));
     m4->get_allocator().construct(&p[4],make_pair<const string,uint>(string("Four"),4));
     m4->get_allocator().construct(&p[5],make_pair<const string,uint>(string("Five"),5));
-    cout<<"ok"<<endl<<"\tThe associations between a 'string' and an 'uint' value in the new allocated array are the following:"<<endl<<endl;
-    for(int i=0;i<6;i++) { cout<<"\tThe string '"<<p[i].first<<"' is associated with the 'uint' value "<<p[i].second<<endl; }
+    cout<<"ok"<<endl<<"\tThe associations between a 'string' and an 'uint' value (stored as pairs) in the new allocated array are the following:"<<endl<<endl;
+    for(int i=0;i<6;i++) 
+    {
+    	cout<<"\t\tThe string '"<<p[i].first<<"' is associated with the 'uint' value "<<p[i].second;
+    	if(i<5) cout<<";"<<endl;
+    	else cout<<"."<<endl;
+    }
+    
+    /* Now, we deallocate the new C-like array 'p'. */
     cout<<endl;
  	cout<<"\tDeallocating the new C-like array ... ";
  	for(int i=0;i<6;i++) m4->get_allocator().destroy(&p[i]);
   	m4->get_allocator().deallocate(p,6);
     cout<<"ok"<<endl<<endl;
  	p=nullptr;
- 	
- 	/* TASK #7 - Creating an empty map 'm5' by using a custom struct for sorting the keys. */
- 	cout<<"\tCreating an empty map 'm5' by using the custom struct for sorting the keys ... ";
+    
+    /* TASK #7 - Creating an empty map 'm5' by using a custom struct for sorting the keys. */
+ 	cout<<"\tCreating an empty map 'm5' by using the 'std::greater<string>' struct for sorting the keys ('strings', in this case) ... ";
  	map<string, uint, std::greater<string> > m5;
  	cout<<"ok"<<endl;
- 	if(m5.empty()==true) cout<<"\tThe new map 'm5' contains no association between a 'string' and an 'uint' value"<<endl;
-    else cout<<"\tThe new map 'm5' contains "<<m5.size()<<" associations between a 'string' and an 'uint' value"<<endl;
-    cout<<"\tAdding several associations between a 'string' and an 'uint' value to the new map 'm5' ... ";
+ 	if(m5.empty()==true) cout<<"\tThe new map 'm5' contains no association between a 'string' and an 'uint' value."<<endl;
+    else cout<<"\tThe new map 'm5' contains "<<m5.size()<<" associations between a 'string' and an 'uint' value."<<endl;
+ 	cout<<"\tAdding several associations between a 'string' and an 'uint' value to the new map 'm5' ... ";
     m5["Seven"]=7;
     m5["Eight"]=8;
     m5["Twelve"]=12;
     m5.emplace_hint(m5.end(),"Seventeen",17);
     cout<<"ok"<<endl;
-    cout<<"\tThe new map 'm5' contains the following "<<m5.size()<<" associations between a 'string' and an 'uint' value"<<endl<<endl;
-    cout<<"\tThe string 'Seven' is associated with the 'uint' value "<<m5.at(string("Seven"))<<endl;
-    cout<<"\tThe string 'Eight' is associated with the 'uint' value "<<m5.at(string("Eight"))<<endl;
-    cout<<"\tThe string 'Twelve' is associated with the 'uint' value "<<m5.at(string("Twelve"))<<endl;
-    cout<<"\tThe string 'Seventeen' is associated with the 'uint' value "<<m5.at(string("Seventeen"))<<endl<<endl;
+    cout<<"\tThe new map 'm5' contains the following "<<m5.size()<<" associations between a 'string' and an 'uint' value:"<<endl<<endl;
+    cout<<"\t\tThe string 'Seven' is associated with the 'uint' value "<<m5.at(string("Seven"))<<";"<<endl;
+    cout<<"\t\tThe string 'Eight' is associated with the 'uint' value "<<m5.at(string("Eight"))<<";"<<endl;
+    cout<<"\t\tThe string 'Twelve' is associated with the 'uint' value "<<m5.at(string("Twelve"))<<";"<<endl;
+    cout<<"\t\tThe string 'Seventeen' is associated with the 'uint' value "<<m5.at(string("Seventeen"))<<"."<<endl<<endl;
     cout.flush();
  	
-    /* TASK 8 - Deallocating all maps, used in this test. */
+ 	/* TASK 8 - Deallocating all maps, used in this test. */
     cout<<"\tDeallocating all maps and other auxiliary data structures in this test ... ";
     m0.clear();
     m1.clear();
@@ -190,5 +213,5 @@ int main(void)
     m5.clear();
     cout<<"ok"<<endl<<endl;
     cout.flush();
-    return EXIT_SUCCESS;
+    return EXIT_SUCCESS;  
 }

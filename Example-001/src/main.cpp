56d24285a938db6c0df1fadd50d9aac70c7c3a38
david <canino.david@gmail.com>
2017-06-01 16:49:42 +0200

    Minor/medium revisions for several tests

--- Example-001/src/main.cpp
+++ Example-001/src/main.cpp
@@ -3,13 +3,16 @@
  *
  * GitHub repository: http://github.com/davidcanino/C_plus_plus_examples
  *
- * Created by David Canino (canino.david@gmail.com), May 2017
+ * Created by David Canino (canino.david@gmail.com), June 2017
  *
  * main.cpp - the source file, implementing the main function for the 'Example-001' Test.
  **********************************************************************************************/
 
 #include <iostream>
 #include <cstdlib>
+#include <climits>
+#include <string>
+#include <cstdio>
 using namespace std;
 
 /// The main function for the <i>'Example-001'</i> Test.
@@ -17,10 +20,9 @@ int main(void)
 {
     /* This is the 'Example-001' Test, near to be the classic 'Hello World!" example! */
     cout<<endl<<"\tThis is the 'Example-001' Test in the C++ language."<<endl<<endl;
-    cout.flush();
 	cout << "\tPress the RETURN key to finish ... ";
 	cout.flush();
-	cin.ignore(UINT_MAX, '\n');
+	getchar();
 	#ifndef _MSC_VER
 		cout << endl;
 		cout.flush();
